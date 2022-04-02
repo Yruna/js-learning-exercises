46f36134059ea609f0e50ef233d82496ea42cc64
Tuna İkin <kayhantunaikin@gmail.com>
2022-04-03 01:23:28 +0300

    hi

---
+++ day1/script.js
@@ -0,0 +1,6 @@
+// single line comment
+
+// Welcome to 30DaysOfJavaScript
+
+/* Comments can make code readable, easy to reuse and informative */
+
---
+++ day1/variables.js
@@ -0,0 +1,28 @@
+let string = 'Hello World'
+let boolean = true //false
+let a 
+let b = null;
+
+let firstUndefiend
+let secondUndefiend
+let thirdUndefiend
+let fourthUndefiend
+
+let firstDefiend = 1
+let secondDefiend = 2
+let thirdDefiend = 3
+let fourthDefiend = 4
+
+/* let firstName = 'Kayhan Tuna'
+let lastName = 'İkin'
+let isMarried = false
+let country = 'Turkey'
+let age = 21 */
+
+let firtsName = 'Kayhan Tuna', lastName = 'İkin', isMarried = false, country = 'Turkey', age = 21
+
+let myAge = 21
+let yourAge = 30
+
+console.log("Ben", myAge, "yaşındayım.")
+console.log("Sen", yourAge, "yaşındasın.")
