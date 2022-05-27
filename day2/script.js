4fba6b547c2dd3810036294267fbd83443735cdd
Tuna İkin <52136614+Yruna@users.noreply.github.com>
2022-05-27 18:57:45 +0300

    Add files via upload
    added day 2 exercises

---
+++ day2/script.js
@@ -0,0 +1,79 @@
+// exercise 1
+
+let challenge = '30 Days of JavaScript';
+
+console.log(challenge);
+
+console.log(challenge.length);
+
+console.log(challenge.toUpperCase());
+
+console.log(challenge.toLowerCase());
+
+console.log(challenge.substring(3,7));
+
+console.log(challenge.substring(3,21));
+
+console.log(challenge.includes("Script"));
+
+console.log(challenge.split(''));
+
+console.log(challenge.split(' '));
+
+console.log('Facebook, Google, Microsoft, Apple, IBM, Oracle, Amazon'.split(','));
+
+console.log(challenge.replace("JavaScript","Python"));
+
+console.log(challenge.charAt(15));
+
+console.log(challenge.charCodeAt(11));
+
+console.log(challenge.indexOf("Days"));
+
+console.log(challenge.lastIndexOf("JavaScript"));
+
+console.log('You cannot end a sentence with because because because is a conjunction'.indexOf('because'));
+
+console.log('You cannot end a sentence with because because because is a conjunction'.lastIndexOf('because'));
+
+console.log('You cannot end a sentence with because because because is a conjunction'.search('because'));
+
+console.log( ' 30 Days Of JavaScript '.trim());
+
+console.log(challenge.startsWith('3'));
+
+console.log(challenge.endsWith('t'));
+
+console.log(challenge.match('a'));
+
+console.log("30 Days of".concat(" JavaScript"));
+
+console.log(challenge.repeat(2));
+
+//exercise 2
+
+console.log("The quote 'There is no exercise better for the heart than reaching down and lifting people up.' by John Holmes teaches us to help one another.");
+
+console.log('"Love is not patronizing and charity isn\'t about pity, it is about love. Charity and love are the same -- with charity you give love, so don\'t just give money but reach out your hand instead."');
+
+console.log(typeof Number('10'));
+
+console.log(Math.ceil(parseFloat('9.8')));
+
+console.log("python".includes("on"));
+
+console.log("jargon".includes("on"));
+
+console.log('I hope this course is not full of jargon'.includes('jargon'));
+
+console.log(Math.floor(Math.random() * 101));
+
+console.log(Math.floor(Math.random() * 51)+50);
+
+console.log(Math.floor(Math.random() * 256));
+
+console.log("1 1 1 1 1\n2 1 2 4 8\n3 1 3 9 27\n4 1 4 16 64\n5 1 5 25 125");
+
+console.log('You cannot end a sentence with because because because is a conjunction'.substring(31, 54));
+
+console.log('Love is the best thing in this world. Some found their love and some are still looking for their love.')
